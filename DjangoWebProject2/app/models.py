1636b86dd8727268243409d85c918f11408ea75a
luis <luis.9993@gmail.com>
2015-05-15 21:26:38 -0300

    Primer cambio

--- DjangoWebProject2/app/models.py
+++ DjangoWebProject2/app/models.py
@@ -5,3 +5,10 @@ Definition of models.
 from django.db import models
 
 # Create your models here.
+
+class Musico(models.Model):
+	nombre = models.CharField(max_length=200)
+	correo = models.CharField(max_length=200)
+	sexo = models.BooleanField(default=True)
+class Banda(models.Model):
+	nombre = models.CharField(max_length=200)
