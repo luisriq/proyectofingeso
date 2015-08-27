84e1dce0ecaac945af127dfb5b8f6cfaeb1739b3
fireness <firenessmax@gmail.com>
2015-08-26 21:32:33 -0300

    agregado modal y la wea

--- previstas/Calendar_config/config_calendar.js
+++ previstas/Calendar_config/config_calendar.js
@@ -5,7 +5,8 @@ $(document).on('click', '.close', function() {
 var events = [ 
     { Title: "Five K for charity", Date: new Date("05/14/2013"), excerpt:"Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet, culpa, tempora sequi labore dicta"}, 
     { Title: "Dinner", Date: new Date("05/31/2013"), excerpt:"Cena de caridad ble"}, 
-    { Title: "Meeting with manager", Date: new Date("05/10/2013"), excerpt:"Cena de caridad ble"}
+    { Title: "Meeting with manager", Date: new Date("05/10/2013"), excerpt:"Cena de caridad ble"},
+    { Title: "El Terrible concierto de rock", Date: new Date("8/28/2015"), excerpt:"El terrible concierto de rock más Épico de todos los tiempos ocurrira en el ..."}
 ];
 
 $("#datepicker").datepicker({
@@ -36,10 +37,13 @@ $("#datepicker").datepicker({
             i++;
         }
             if (event) {
-              
-                $(".eventdata").empty();
+                $('.titulo-evento-modal').html("<h4>"+event.Title+"</h4>");
+                $('.descripcion-evento-modal').html(event.excerpt);
+                $('#modal1').openModal();
+                /*$(".eventdata").empty();
                 $(".over").fadeIn("slow");
-                $(".eventdata").append("<h1>"+event.Title+"</h1><p>"+event.excerpt+"</p>");
+                $(".eventdata").append("<h1>"+event.Title+"</h1><p>"+event.excerpt+"</p>");*/
+                
             }
     }
 
