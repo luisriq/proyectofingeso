c0cc6ba4008e2468764f168eb16fb0d42488679c
luis <luis.9993@gmail.com>
2015-09-14 21:04:41 -0300

    js

--- DjangoWebProject2/app/static/app/scripts/solicitudes.js
+++ DjangoWebProject2/app/static/app/scripts/solicitudes.js
@@ -58,7 +58,30 @@ $( document ).ready(function(){
 							$('.twitter-container').html('<a class="twitter-timeline" style="width:100%" href="https://twitter.com/Crunchyroll" data-widget-id="634820916141289472" data-screen-name="'+dato.val()+'"></a>');
 							twttr.widgets.load()
 							console.log("holi");
-						}else if(formulario.attr('data-target')=="instrumento")
+						}else if(formulario.attr('data-target')=="solicitar"){
+							//TODO: Cambiar html 
+							$('#botonSolicitar').replaceWith('<a class="col hover-shadow s5 offset-s2 card-panel color-principal white-text disabled" style="padding:10px">Solicitado</a>');
+						}
+						else if(formulario.attr('data-target')=="aceptarSolicitud"){
+							console.log("MEEEE");
+							formulario.hide();
+							if(olddato.val()=='aceptar'){
+								hrefItem = formulario.find('.valign-wrapper').attr('href');
+								imagen = formulario.find('.circle').attr('style');
+								nombre = formulario.find('span').text()
+								console.log(hrefItem+"\n"+imagen);
+								$('#integrante').append('<li class="collection-item valign-wrapper">'+
+												'<a class="valign-wrapper" href="'+hrefItem+'">'+
+													'<div class="circle avatar-perfil small" style="'+imagen+'" ></div>'+
+													'<div class="" style="margin-left:20px;"><span >'+nombre+'</span>'+
+													'<br><span class="grey-text ">S</span></div>'+
+												'</a>'+
+											'</li>');
+							}
+							
+							
+						}
+						else if(formulario.attr('data-target')=="instrumento")
 							location.reload();
 						var txtconbr=dato.val().replace(/(?:\r\n|\r|\n)/g, '<br />').trim();
 						formulario.find(".dato").html(txtconbr.capitalizeFirstLetter());
@@ -80,129 +103,8 @@ $( document ).ready(function(){
 		else
 			Materialize.toast('<span class="yellow-text"><i class="material-icons">&#xE002;</i></span>Las palabras no pueden superar '+formulario.attr("largomaximo")+' caracteres', 4000);
 	});
-	
-	$('.addInstrumento').click(function(){
-		$('#modal1').openModal();
-	});
-	$('input[type=range]').change(function(){
-		var img = $(this).parent().parent().find(".card-image").find('img');
-		var path = $(this).attr("data-img-url");
-		var token = $(this).attr("data-token");
-		var value=$(this).val()
-		//cambiar en la bd
-		$.ajax({
-			url : "/guardarDatosArtista", // the endpoint
-			type : "POST", // http method
-			data : { dato : $(this).val(),
-					idToca: $(this).attr("data-id"),
-					target : 'iNivel',
-				"X-CSRFToken" : token }, // data sent with the post request
-			// handle a successful response
-			success : function(response) {
-				console.log(response); // log the returned json to the console
-				if(response=="OK"){
-					console.log("servidor responde OK")
-					switch (value){
-						case "1":
-							img.attr("src",path+"1.jpg");
-							break; 
-						case "2":
-							img.attr("src",path+"2.jpg");
-							break; 
-						case "3":
-							img.attr("src",path+"3.jpg");
-							break; 
-						case "4":
-							img.attr("src",path+"4.jpg");
-							break; 
-						case "5":
-							img.attr("src",path+".gif");
-							break; 
-					}
-				}
-				else{
-					Materialize.toast(' <span class="red-text"><i class="material-icons">&#xE14C;</i></span>Error al cambiar el nivel del instrumento', 4000);
-					console.log("error en el servidor");
-				}
-			},
-			// handle a non-successful response
-			error : function(xhr,errmsg,err) {
-				Materialize.toast('<span class="red-text"><i class="material-icons">&#xE14C;</i></span>Error al cambiar el nivel del instrumento ', 4000);
-				console.log("error en la comunicación");
-			}
-		});
-		console.log("jakdjfsba")
-	});
 	//hay que hacer que sea mas elegante... meh
-	$('.ajax-file-selection').click(function(){
-		input = $('.file-form').find('input[type=file]');
-		input.trigger('click'); 
-	});
-	$('.file-form input[type=file]').change(function(){
-		readURL(this);
-		$('.fileChange').toggle();
-	});
-	$('.fileChange.cancel').click(function(){
-		$('#preview').attr('src', $('#preview').attr('data-src'));
-		$('.fileChange').toggle();
-	});
-	$('.ajax-file').click(function(){
-		form = $('.file-form');
-		var formData = new FormData(form[0]);
-		if(form.find('input[type=file]').val()!=''){
-			$.ajax({
-				url: form.attr('action'),
-				type: 'POST',
-				data: formData,
-				cache: false,
-				contentType: false,
-				processData: false,
-				success: function (returndata) {
-					$('.myavatar img').attr('src',"/media/"+returndata);
-					Materialize.toast('<span class="green-text"><i class="material-icons">&#xE5CA;</i></span>Imagen de perfíl cambiada con exito', 4000);
-					$('.fileChange').toggle();
-					$('#preview').attr('data-src',"/media/"+returndata);
-				},
-				error: function (returndata) {
-					$('#preview').attr('src', $('#preview').attr('data-src'));
-					Materialize.toast('<span class="red-text"><i class="material-icons">&#xE14C;</i></span>Error al cambiar Imagen de perfíl', 4000);
-					$('.fileChange').toggle();
-				},
-			});
-		}else{
-			Materialize.toast('<span class="yellow-text"><i class="material-icons">&#xE002;</i></span>Debes seleccionar una imagen', 4000);
-		}
-	})
-	//fin de cambio archivo
-	$('.del-instrumento').click(function(){
-		var this_= $(this);
-		var token = $(this).attr("data-token");
-		$.ajax({
-			url : "/guardarDatosArtista", // the endpoint
-			type : "POST", // http method
-			data : { dato : $(this).val(),
-					idToca: $(this).attr("data-id"),
-					target : 'delToca',
-				"X-CSRFToken" : token }, // data sent with the post request
-			// handle a successful response
-			success : function(response) {
-				if(response=="OK"){
-					Materialize.toast('<span class="green-text"><i class="material-icons">&#xE5CA;</i></span>Ya no tocas ese instumento', 4000);
-					this_.parent().remove();
-				}
-				else if(response="ERROR")
-					Materialize.toast('<span class="red-text"><i class="material-icons">&#xE14C;</i></span>Error al eliminar el instrumento', 4000);
-			},
-			// handle a non-successful response
-			error : function(xhr,errmsg,err) {
-				Materialize.toast('<span class="red-text"><i class="material-icons">&#xE14C;</i></span>Error al eliminar el instrumento', 4000);
-				console.log("error en la comunicación");
-			}
-		});
-	});
 	
-	
-	twitLoad();
 	for(var i=1;i<20;i++)
 		setTimeout(function(){
 			if($('iframe').width()>$('iframe').parent().width()){
@@ -229,26 +131,6 @@ function largoPalabra(texto, maximo){
 	return g;
 }
 
-
-function twitLoad(){
-	window.twttr = (function(d, s, id) {
-	var js, fjs = d.getElementsByTagName(s)[0],
-		t = window.twttr || {};
-	if (d.getElementById(id)) return t;
-	js = d.createElement(s);
-	js.id = id;
-	js.src = "https://platform.twitter.com/widgets.js";
-	fjs.parentNode.insertBefore(js, fjs);
-	
-	t._e = [];
-	t.ready = function(f) {
-		t._e.push(f);
-	};
-	
-	return t;
-	}(document, "script", "twitter-wjs"));
-	
-}
 
 function readURL(input) {
     if (input.files && input.files[0]) {
