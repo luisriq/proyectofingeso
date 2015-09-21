4cb521591a6fefdbe0d6c7c3b166ac7169f3f1c3
fireness <firenessmax@gmail.com>
2015-09-21 13:41:54 -0300

    Cambios

--- DjangoWebProject2/app/tests.py
+++ DjangoWebProject2/app/tests.py
@@ -8,26 +8,32 @@ from django.test import TestCase
 
 # TODO: Configure your database in settings.py and sync before running tests.
 
-class ViewTest(TestCase):
-    """Tests for the application views."""
+#class ViewTest(TestCase):
+#    """Tests for the application views."""
 
-    if django.VERSION[:2] >= (1, 7):
-        # Django 1.7 requires an explicit setup() when running tests in PTVS
-        @classmethod
-        def setUpClass(cls):
-            django.setup()
+#    if django.VERSION[:2] >= (1, 7):
+#        # Django 1.7 requires an explicit setup() when running tests in PTVS
+#        @classmethod
+#        def setUpClass(cls):
+#            django.setup()
 
-    def test_home(self):
-        """Tests the home page."""
-        response = self.client.get('/')
-        self.assertContains(response, 'Home Page', 1, 200)
+#    def test_home(self):
+#        """Tests the home page."""
+#        response = self.client.get('/')
+#        self.assertContains(response, 'Home Page', 1, 200)
 
-    def test_contact(self):
-        """Tests the contact page."""
-        response = self.client.get('/contact')
-        self.assertContains(response, 'Contact', 3, 200)
+#    def test_contact(self):
+#        """Tests the contact page."""
+#        response = self.client.get('/contact')
+#        self.assertContains(response, 'Contact', 3, 200)
 
-    def test_about(self):
-        """Tests the about page."""
-        response = self.client.get('/about')
-        self.assertContains(response, 'About', 3, 200)
+#    def test_about(self):
+#        """Tests the about page."""
+#        response = self.client.get('/about')
+#        self.assertContains(response, 'About', 3, 200)
+
+class Testing(TestCase):
+    def test_login(self):
+        resp = self.client.get('/login/')
+        self.assertEqual(resp.status_code, 200)
+
